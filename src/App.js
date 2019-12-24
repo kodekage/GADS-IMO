7ef23b4e13b35325d28632affc1c8ab8a7c6f924
Opara Prosper <oparaprosper79@gmail.com>
2019-12-24 16:12:24 +0100

    Add App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import VideoComponent from './videoComponent';
+import LCAComponent from './lcaComponent';
+import LearnersComponent from './learnersComponent';
+import HeadersComponent from './headerComponent';
+import Button from './button';
 import './App.css';
 
 function App() {
+  // const style = {
+    // display: 'block',
+    // width: '500px',
+    // height: '350px',
+    // margin: '5em auto 0',
+    // border: '2px solid red'
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        {/* <HeadersComponent/> */}
+        <div className="container text-left app-content">
+          <h1>Google Africa <br/>Developer Scholarship</h1>
+          <h2>ALC : Imo State</h2>
+          {/* <img className="mx-auto d-block" style={style} src={gads} alt="alc_logo"></img> */}
+          <Button link_to="meetup highlight" bg_color="#3359DF"/>
+        </div>
+      </div>
+
+      <div className="container-fluid">
+        <VideoComponent/>
+      </div>
+
+      <div className="body">
+        <div className="container">
+          <LCAComponent/>
+          <LearnersComponent/>
+        </div>
+      </div>
     </div>
   );
 }

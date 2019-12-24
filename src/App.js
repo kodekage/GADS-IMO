95e4ddaccd1704531d6b48c517c4eeb64ead2adc
Opara Prosper <oparaprosper79@gmail.com>
2019-12-24 18:17:07 +0100

    Add animation on component mount

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import LCAComponent from './lcaComponent';
 import LearnersComponent from './learnersComponent';
 import HeadersComponent from './headerComponent';
 import Button from './button';
+import sr from './ScrollReveal';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  componentDidMount(){
+    sr.reveal(this.refs.box1, {delay: 2500});
+    // sr.reveal(this.refs.box2, {delay: 2500});
+    sr.reveal(this.refs.text1, {delay: 500});
+    sr.reveal(this.refs.text2, {delay: 1000});
+    sr.reveal(this.refs.text3, {delay: 1500});
+    sr.reveal(this.refs.text4, {delay: 2000});
+  };
   // const style = {
     // display: 'block',
     // width: '500px',
@@ -14,30 +23,33 @@ function App() {
     // margin: '5em auto 0',
     // border: '2px solid red'
   // }
-  return (
-    <div>
-      <div className="App">
-        {/* <HeadersComponent/> */}
-        <div className="container text-left app-content">
-          <h1>Google Africa <br/>Developer Scholarship</h1>
-          <h2>ALC : Imo State</h2>
-          {/* <img className="mx-auto d-block" style={style} src={gads} alt="alc_logo"></img> */}
-          <Button link_to="meetup highlight" bg_color="#3359DF"/>
+  render() {
+    return (
+      <div>
+        <div className="App">
+          {/* <HeadersComponent/> */}
+          <div className="container text-left app-content">
+            <h1><span ref="text1">Google</span> <span ref="text2">Africa</span> <br/>
+            <span ref="text3">Developer</span> <span ref="text4">Scholarship</span></h1>
+            <h2 ref="box1">ALC : Imo State</h2>
+            {/* <img className="mx-auto d-block" style={style} src={gads} alt="alc_logo"></img> */}
+            <Button ref="box2" link_to="meetup highlight" bg_color="#3359DF"/>
+          </div>
+        </div>
+      
+        <div className="container-fluid">
+          <VideoComponent/>
+        </div>
+      
+        <div className="body">
+          <div className="container">
+            <LCAComponent/>
+            <LearnersComponent/>
+          </div>
         </div>
       </div>
-
-      <div className="container-fluid">
-        <VideoComponent/>
-      </div>
-
-      <div className="body">
-        <div className="container">
-          <LCAComponent/>
-          <LearnersComponent/>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;

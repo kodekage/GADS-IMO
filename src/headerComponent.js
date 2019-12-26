562d667fd2c8c419f17f391c850678689f253148
Opara Prosper <oparaprosper79@gmail.com>
2019-12-26 14:50:34 +0100

    Add meetup highlight link

--- src/headerComponent.js
+++ src/headerComponent.js
@@ -5,9 +5,11 @@ import gdg from './img/gdg.png'
 export default function HeaderComponent(props) {
   return (
     <header>
-      {/* <img src={gdg} alt="ALC logo"/> */}
-
       <ul>
+        <li>
+          <a href="#">Meetup Highlight</a>
+        </li>
+
         <li>
           <a href="#">LCA'S</a>
         </li>

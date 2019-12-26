88312f8b2df58a7bfbaae378d8d3ca7a2f56347e
Opara Prosper <oparaprosper79@gmail.com>
2019-12-26 14:51:04 +0100

    Refactor

--- src/App.js
+++ src/App.js
@@ -16,24 +16,17 @@ class App extends React.Component {
     sr.reveal(this.refs.text3, {delay: 1500});
     sr.reveal(this.refs.text4, {delay: 2000});
   };
-  // const style = {
-    // display: 'block',
-    // width: '500px',
-    // height: '350px',
-    // margin: '5em auto 0',
-    // border: '2px solid red'
-  // }
+  
   render() {
     return (
       <div>
         <div className="App">
-          {/* <HeadersComponent/> */}
+          <HeadersComponent/>
           <div className="container app-content">
-            <h1><span ref="text1">Google</span> <span ref="text2">Africa</span>
+            <h1><span ref="text1">Google</span> <span ref="text2">Africa</span><br/>
             <span ref="text3"> Developer</span> <span ref="text4">Scholarship</span></h1>
-            <h2 ref="box1">ALC : Imo State</h2>
-            {/* <img className="mx-auto d-block" style={style} src={gads} alt="alc_logo"></img> */}
-            <Button ref="box2" link_to="meetup highlight" bg_color="#3359DF" font_color="#ffffff"/>
+            <h2 ref="box1">#ALCImo</h2>
+            {/* <Button ref="box2" link_to="meetup highlight" bg_color="#3359DF" font_color="#ffffff"/> */}
           </div>
         </div>
       

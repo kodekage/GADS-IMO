d265b5cba9aff7681ce30d5b20bca2205803f33b
Opara Prosper <oparaprosper79@gmail.com>
2019-12-27 14:27:36 +0100

    Remove links

--- src/headerComponent.js
+++ src/headerComponent.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import gads from './img/alc_logo.png';
-import gdg from './img/gdg.png'
+// import gdg from './img/gdg.png'
 
 export default function HeaderComponent(props) {
   return (
     <header>
-      <ul>
-        {/* <a href="#"> */}
-          {/* <li>Meetup highlight</li> */}
-        {/* </a> */}
-
-        <a href="#">
-          <li>LCA's</li>
-        </a>
-
-        <a href="#">
-          <li>Scholars</li>
-        </a>
-      </ul>
+      <img src={gads} alt="GDG Logo"/>
     </header>
   );
 }

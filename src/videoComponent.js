6595f12abfea1e01eac97ec653db48cbff17500d
Opara Prosper <oparaprosper79@gmail.com>
2019-12-28 23:59:12 +0100

    Add video overlay feature
    the highlight video overlays the entire dom
    and this enhances users experience by making
    users focus on the video and less of other
    site components, thereby absorbing our story

--- src/videoComponent.js
+++ src/videoComponent.js
@@ -1,14 +1,50 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHamburger } from '@fortawesome/free-solid-svg-icons'
+import playImg from './img/play-img.png';
 import './Video.css';
 
-function VideoComponent() {
-  return (
-    <div id="videoComponent">
-        <iframe className="iframe" height="600" src="https://www.youtube.com/embed/VjzXR3K89z8"
-        frameborder="5" allow="accelerometer; autoplay;
-        encrypted-media; gyroscope; picture-in-picture" rel="0"></iframe>
-   </div>
-  )
-}
+export default class VideoComponent extends React.Component {
+  state = {
+    style: {
+      display: 'none'
+    }
+  }
 
-export default VideoComponent;
+  hideVideo(event) {
+    this.setState({
+      style: {
+        display: 'none'
+      }
+    })
+  }
+
+  showVideo(event) {
+    event.preventDefault();
+
+    this.setState({
+      style: {
+        display: 'block'
+      }
+    })
+  }
+  
+  render() {
+    return (
+      <div id="videoComponent">
+        <a onClick={this.showVideo.bind(this)} href="https://www.youtube.com/embed/VjzXR3K89z8">
+          <img className="dummyIframe" src={playImg} alt="Play Image"/>
+        </a>
+
+        <div className="iframe" style={this.state.style}>
+          <FontAwesomeIcon icon={faHamburger} onClick={this.hideVideo.bind(this)} style={{cursor: 'pointer'}}/>
+          <iframe width="1351" height="502" src="https://www.youtube.com/embed/VjzXR3K89z8"
+          frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen>
+
+          </iframe>
+        </div>
+      </div>
+    )
+  }
+}

f5d75e717ed2114dc393771564e9a307306b6290
Opara Prosper <oparaprosper79@gmail.com>
2019-12-29 15:51:41 +0100

    Refactor

--- src/videoComponent.js
+++ src/videoComponent.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHamburger } from '@fortawesome/free-solid-svg-icons'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import playImg from './img/play-img.png';
 import './Video.css';
 
 export default class VideoComponent extends React.Component {
   state = {
     style: {
-      display: 'none'
+      visibility: 'hidden'
     }
   }
 
   hideVideo(event) {
     this.setState({
       style: {
-        display: 'none'
+        visibility: 'hidden'
       }
-    })
+    });
   }
 
   showVideo(event) {
@@ -24,27 +24,26 @@ export default class VideoComponent extends React.Component {
 
     this.setState({
       style: {
-        display: 'block'
+        visibility: 'visible'
       }
-    })
+    });
   }
   
   render() {
     return (
       <div id="videoComponent">
         <a onClick={this.showVideo.bind(this)} href="https://www.youtube.com/embed/VjzXR3K89z8">
-          <img className="dummyIframe" src={playImg} alt="Play Image"/>
+          <img className="dummyIframe" src={playImg} alt="Play button"/>
         </a>
 
         <div className="iframe" style={this.state.style}>
-          <FontAwesomeIcon icon={faHamburger} onClick={this.hideVideo.bind(this)} style={{cursor: 'pointer'}}/>
-          <iframe width="1351" height="502" src="https://www.youtube.com/embed/VjzXR3K89z8"
-          frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          <FontAwesomeIcon icon={faTimesCircle} onClick={this.hideVideo.bind(this)} style={{cursor: 'pointer'}}/>
+          <iframe title="ALCIMO meetup highlight" width="1351" height="502" src="https://www.youtube.com/embed/VjzXR3K89z8"
+          frameborder="0" allow="accelerometer; autoplay=0; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen>
-
           </iframe>
         </div>
       </div>
-    )
+    );
   }
 }

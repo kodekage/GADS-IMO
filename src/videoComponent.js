2bfdd8ca99a7d8f4e24e2d9841f5b2384baa0a7b
Opara Prosper <oparaprosper79@gmail.com>
2019-12-31 13:11:21 +0100

    Add new image

--- src/videoComponent.js
+++ src/videoComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import YouTube from 'react-youtube';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
-import playImg from './img/play-img.png';
+import gads_thumbnail from './img/gads_thumbnail.png';
 import './Video.css';
 
 export default class VideoComponent extends React.Component {
@@ -44,7 +44,7 @@ export default class VideoComponent extends React.Component {
     return (
       <div id="videoComponent">
         <a onClick={this.showVideo.bind(this)} href="https://www.youtube.com/embed/VjzXR3K89z8">
-          <img className="dummyIframe" src={playImg} alt="Play button"/>
+          <img className="dummyIframe" src={gads_thumbnail} alt="Play button"/>
         </a>
 
         <div className="iframe" style={this.state.style}>

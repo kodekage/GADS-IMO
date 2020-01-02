50d2be0508d62285690845bd392bcd45d309d309
Opara Prosper <oparaprosper79@gmail.com>
2020-01-02 22:39:45 +0100

    Refactor

---
+++ src/lcaCardComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from './button';
+import alc from './img/lca.jpg';
+
+export default function LCAComponent() {
+  const img = {
+    background: `linear-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.3)), url(${alc})`,
+    backgroundPostion: 'center',
+    backgroundSize: 'cover',
+  }
+
+  return (
+    <div className="row lca-component">
+      <div className="col-lg-4 align-self-center lca-component-description">
+        <h2>LCA's</h2>
+        <p>
+          Learning Community Ambassador's are volunteers that
+          give back to the Community
+        </p>
+        <Button width="60%" link="/lcas" link_to="LCA's profile" bg_color="#FFAF30" font_color="#111111"/>
+      </div>  
+
+      <div className="col-lg-8 component-bg" style={img}>
+
+      </div>
+    </div>
+  );
+}

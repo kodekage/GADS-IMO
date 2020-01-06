521c8c2baa9a391d7bc43e55e6e5899404c99c0f
Opara Prosper <oparaprosper79@gmail.com>
2020-01-06 16:45:55 +0100

    Add learners Component and styling

--- src/learnerBio.js
+++ src/learnerBio.js
@@ -1,32 +1,83 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGlobeAfrica, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import scholar from './img/learner.jpg';
 import './learnerBio.css';
 
-export default function LearnerBio(props) {
-  const style = {
-    icon: {
-      // border: '1px solid #fff',
-      marginLeft: '95%',
-      cursor: 'pointer'
-    }
+export default class LearnerBio extends React.Component {
+
+  state = {
+    style: {
+      display: 'none'
+    },
+    id: this.props.id
   }
 
-  return (
-    <div className="col-xs-1 col-sm-4 col-md-4 col-lg-2 learnerBio">
-      <div>
-        <img src={scholar} alt="AlC Imo scholar" onClick={props.showBio}/>
-      </div>
-      
-      <div className="briefBio" style={props.show}>
-        <div className="item">
-          <FontAwesomeIcon style={style.icon} icon={faTimesCircle} onClick={props.hideBio}/>
-          <h3>Opara Prosper</h3>
-          <p>Mobile web specialist</p>
-          <FontAwesomeIcon icon={faGlobeAfrica}/>
+  showBio(event) {
+    this.setState({
+      style: {
+        display: 'block'
+      }
+    })
+  }
+
+  hideBio(event) {
+    this.setState({
+      style: {
+        display: 'none'
+      }
+    })
+  }
+
+  render() {
+    const style = {
+      icon: {
+        // border: '1px solid #fff',
+        marginLeft: '95%',
+        cursor: 'pointer'
+      },
+      p: {
+        color: '#FFAF30',
+        fontSize: '15px'
+      },
+      a: {
+        color: '#444'
+      }
+    }
+  
+    return (
+      <div className="col-xs-1 col-sm-4 col-md-4 col-lg-3">
+        <div className="learnerBio">
+          <div>
+            <img src={this.props.img} alt="AlC Imo scholar"/>
+            <div>
+              <h3 style={{fontSize: '25px', fontWeight: '600'}}>{this.props.name}</h3>
+              <p style={style.p}>{this.props.track} track</p>
+              <a style={style.a} href={this.props.github}><FontAwesomeIcon icon={faGithub} style={{fontSize: '30px'}}/></a>
+              <span onClick={this.showBio.bind(this)}>MORE +</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="briefBio" style={this.state.style}>
+          <div className="item">
+            <FontAwesomeIcon style={style.icon} icon={faTimesCircle} onClick={this.hideBio.bind(this)}/>
+            <h3 style={{fontSize: '25px', fontWeight: '600'}}>{this.props.name}</h3>
+            <p style={style.p}>{this.props.track} track</p>
+            <div className="row">
+              <div className="col-lg-4">
+                <img src={this.props.img} alt="AlC Imo scholar"/>
+                {/* <FontAwesomeIcon icon={faGithub} style={{display: 'block', margin: '.5em 0', fontSize: '30px'}}/> */}
+              </div>
+
+              <div className="col-lg-8">
+                <p><i>{this.props.bio}</i></p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/learnersComponent.js
+++ src/learnersComponent.js
@@ -4,34 +4,64 @@ import LearnerBio from './learnerBio';
 import './learnerBio.css';
 
 export default class LearnersComponent extends React.Component {
-  state = {
-    show: {
-      display: 'none',
-    }
-  }
+  constructor(props) {
+    super(props);
 
-  showBio(event) {
-    console.log(event.target.img)
-    this.setState({
-      show: {
-        display: 'block'
+    this.scholars = [
+      {
+        name: 'Ndubuisi Ugwulo J.',
+        img: 'https://avatars2.githubusercontent.com/u/52312550?s=400&v=4',
+        bio: `Alright, I am still in the learning process and on the road to
+              becoming a world class Software Developer. I am very passionate
+              about tech and its impact in the world today, this is why I delved
+              into software development to leverage this opportunity to effect a
+              positive change in my own little way and to make life easier for
+              the world at large.`,
+        track: 'Associate Android Developer',
+        github: 'https://github.com/ugwulo'
+      },
+      {
+        name: 'Sixtus Onumajuru',
+        img: 'https://avatars0.githubusercontent.com/u/15157786?s=400&v=4',
+        bio: `I am and Eccentric person when it comes to programming and things in
+              general, I find it hard to focus on one programming language so I am
+              doing a lot athe same time but I see myself as a Software Developer
+              focusing on The MEAN/LAMP Stack and Android/Java.`,
+        track: 'Mobile Web Specialist',
+        github: 'https://github.com/jiggabyte'
+      },
+      {
+        name: 'Johnpaul Uzukwu',
+        img: 'https://avatars0.githubusercontent.com/u/52359007?s=400&v=4',
+        bio: `Am a back-end engineer, proficient in php and Mysql`,
+        track: 'Mobile Web Specialist',
+        github: 'https://github.com/uzukwujp'
+      },
+      {
+        name: 'Onuohb Godfirst Chibuzo',
+        img: 'https://avatars0.githubusercontent.com/u/52785343?s=460&v=4',
+        bio: `I'm proficient in Mobile Development`,
+        track: 'Associate Android Developer',
+        github: 'https://www.github.com/chibufirst'
       }
-    })
-  }
-
-  hideBio() {
-    this.setState({
-      show: {
-        display: 'none'
-      }
-    })
+    ]
   }
 
   render() {
     let learner = [];
 
-    for(let x=0; x < 10; x++) {
-      learner.push(<LearnerBio key={x} show={this.state.show} showBio={this.showBio.bind(this)} hideBio={this.hideBio.bind(this)}/>);
+    for(let x=0; x < this.scholars.length; x++) {
+      learner.push(
+        <LearnerBio
+        key={x}
+        id={x}
+        name={this.scholars[x].name}
+        track={this.scholars[x].track}
+        img={this.scholars[x].img}
+        bio={this.scholars[x].bio}
+        github={this.scholars[x].github}
+        />
+      );
     }
 
     
@@ -41,21 +71,23 @@ export default class LearnersComponent extends React.Component {
         <div className="container">
           <h2>ALC IMO LEARNERS</h2>
 
-          <div className="row no-gutters align-items-center justify-content-center tracks">
-            <div className="col-lg-4">
-              <button>Mobile Web Specialist</button>
-            </div>
+          {/* <div className="row no-gutters align-items-center justify-content-center tracks"> */}
+            {/* <div className="col-lg-4"> */}
+              {/* <button>Mobile Web Specialist</button> */}
+            {/* </div> */}
+{/*  */}
+            {/* <div className="col-lg-4"> */}
+              {/* <button>Associate Android Developers</button> */}
+            {/* </div> */}
+{/*  */}
+            {/* <div className="col-lg-3"> */}
+              {/* <button>Cloud Engineers</button> */}
+            {/* </div> */}
+          {/* </div> */}
+{/*  */}
+          {/* <div className="breadcrump">Learners/all</div> */}
 
-            <div className="col-lg-4">
-              <button>Associate Android Developers</button>
-            </div>
-
-            <div className="col-lg-3">
-              <button>Cloud Engineers</button>
-            </div>
-          </div>
-
-          <div className="row no-gutters justify-content-center learnersList">
+          <div className="row no-gutters justify-content-left learnersList">
            { learner.map((item) => item) }
           </div>
         </div>
